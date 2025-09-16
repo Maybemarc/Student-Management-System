5d729b7f7a4c99ae1b638b02e98ecad0c0b685d1
Maybemarc <marcspectre777@gmail.com>
2025-09-16 21:21:43 +0530

    updates

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -76,7 +76,7 @@ export const login = async (req, res) => {
     res.cookie("token", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: process.env.NODE_ENV === "production" ? "strict" : "lax",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
       maxAge: 10 * 24 * 60 * 60 * 1000,
     });
 
